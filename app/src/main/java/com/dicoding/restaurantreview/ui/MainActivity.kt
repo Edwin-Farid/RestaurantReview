fe1650fcdd2e0a47b3f717e4f56860a2df7f3e34
Edwin-Farid <117edwin@gmail.com>
2023-09-29 16:55:17 +0800

    feat: add view binding & restaurant response

--- app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dicoding.restaurantreview
+package com.dicoding.restaurantreview.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.restaurantreview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
