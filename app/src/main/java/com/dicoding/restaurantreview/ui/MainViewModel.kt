1361c41cf7234dd3e45ed1d7149de681bcccde99
Edwin-Farid <117edwin@gmail.com>
2023-10-01 07:06:09 +0800

    feat: add logic in Main View Model

--- app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
+++ app/src/main/java/com/dicoding/restaurantreview/ui/MainViewModel.kt
@@ -1,4 +1,23 @@
 package com.dicoding.restaurantreview.ui
 
-class MainViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dicoding.restaurantreview.data.response.CustomerReviewsItem
+import com.dicoding.restaurantreview.data.response.Restaurant
+
+class MainViewModel : ViewModel() {
+    private val _restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewsItem>>()
+    val listReview: LiveData<List<CustomerReviewsItem>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
 }
